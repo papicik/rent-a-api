559ae7f453fa97a3792b5c749871be128a03bdc9
canmotro <canmotro@users.noreply.github.com>
2026-09-15 02:01:33 +0300

    fix(deploy): add prisma generate to build/postinstall and ignore eslint on vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { webpack }) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
